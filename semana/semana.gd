409730d43d9fd01aa90ffa7cd05a11f7ca6b2980
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2026-08-19 00:06:02 -0300

    Escena menu e intro

--- semana/semana.gd
+++ semana/semana.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-var faltasStart: float = 5
+@export var faltasStart: int
 var faltas: float
+var faltasCartel: Label
 
 var horasDeEstudio: Array
 
 func _ready():
 	faltas = faltasStart
+	faltasCartel = $Faltas
+	cambiarFaltas(0)
 	
 
-func cambiarFaltas(cambio: float):
+func cambiarFaltas(cambio: int):
 	faltas += cambio
+	faltasCartel.text = "Quedan " + str(int(faltas)) + " faltas"
 	
 
 func _on_hora_estudiando(materia, clase, estudiada):
@@ -21,3 +25,9 @@ func _on_hora_3_estudiando(materia, clase, estudiada):
 	estudiando(materia, clase, estudiada)
 func estudiando(materia, clase, estudiada):
 	print(str(materia) + " " + str(clase) + " " + str(estudiada))
+	
+	if clase >= 0:
+		cambiarFaltas(-estudiada)
+
+func _on_terminar_pressed():
+	pass # Replace with function body.
