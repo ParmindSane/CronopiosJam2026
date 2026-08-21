c1007028a463e996eeb4b9e624ef06c8b42e4771
FerCardosMartegani <103231341+FerCardosMartegani@users.noreply.github.com>
2026-08-20 23:47:23 -0300

    Libros respawnean ordenados

--- semana/libro.gd
+++ semana/libro.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var id: int
 var materia: int
 var sprite: Sprite2D
 
@@ -9,9 +10,11 @@ func _ready():
 	sprite = $Sprite2D
 	
 
-func setMateria(num:int):
-	materia = num
+func setMateria(_materia:int, _id: int):
+	id = _id
+	materia = _materia
 	sprite.self_modulate = Color.from_hsv(Global.map(materia, -1,3, 40,360)/360, 1,1)
+	
 
 func _on_draggable_drag_started(area):
 	emit_signal("libroAgarrado", self)
--- semana/libro_start.gd
+++ semana/libro_start.gd
@@ -9,18 +9,18 @@ var libros: Array
 
 func _ready():
 	for i in range(cantidadTotal):
-		addLibro()
+		addLibro(i)
 	
 
-func addLibro():
-		var libroNuevo = libroRef.instantiate()
-		libros.push_back(libroNuevo)
-		add_child(libroNuevo)
-		libroNuevo.position = Vector2(0,0)
-		libroNuevo.setMateria(materia)
+func addLibro(i: int):
+	var libroNuevo = libroRef.instantiate()
+	libros.push_back(libroNuevo)
+	add_child(libroNuevo)
+	libroNuevo.position = Vector2(Global.map(i, 0,cantidadTotal-1, -1, +1)*140,0)
+	libroNuevo.setMateria(materia, i)
 	
 
 func _on_drop_zone_drop_applied(zone, area, plan):
 	if area.materia == materia:
-			area.queue_free()
-			addLibro()
+		addLibro(area.id)
+		area.queue_free()
